1d15e7b031da48eb04122bb1e80608909459b0c0
Alex Scott <codetheorist@gmail.com>
2017-11-15 17:47:09 +0000

    Add  and __construvt to Restaurant class

--- src/Restaurant.php
+++ src/Restaurant.php
@@ -2,7 +2,25 @@
 
 namespace TakeawayTown\LaravelRestaurant;
 
+use Illuminate\Support\Facades\Config;
+
 class Restaurant
 {
+    /**
+     * Our Laravel Application
+     *
+     * @var \Illuminate\Foundation\Application
+     */
+    public $app;
+
+    /**
+     * Create a new Teamwork instance.
+     *
+     * @param \Illuminate\Foundation\Application $app
+     */
+    public function __construct( $app )
+    {
+        $this->app = $app;
+    }
 
 }
